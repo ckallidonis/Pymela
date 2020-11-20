20355eea55061373cbed5c54be00f322313df41d
Christos Kallidonis <kallidonis@gmail.com>
2020-11-20 14:38:42 -0500

    Add the file path to sys.path, command line option to pass the JSON input file

--- tests/read_2pt_corr.py
+++ tests/read_2pt_corr.py
@@ -1,13 +1,31 @@
 import sys, os
-sys.path.insert(len(sys.path), '../pymela/')
+import optparse
 
+# Add package path to sys.path. This allows us to run this tests script from any directory, without import issues
+file_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(file_path+'/../')
+
+# Avoid writing the compiled files
+sys.dont_write_bytecode = True
+
+# Import local modules
 import pymela.io.json_io as JSONio
 
-iniDict = JSONio.parse('/Users/kallidoc/work_local/pdf_distillation/analysis/scratch/test_json.py')
 
+# Parse command line options
+usage = "usage: %prog [options] "
+opt_parser = optparse.OptionParser(usage)
+
+opt_parser.add_option("-i", "--input_file", type="string", default='',
+                  help='Input file in JSON format')
+
+(options, args) = opt_parser.parse_args()
+
+input_file=options.input_file
+if input_file == '':
+    raise ValueError('--input_file option must be set')
+
+
+# Parse and print out the input file
+iniDict = JSONio.parse(input_file)
 JSONio.dump(iniDict)
-
-if iniDict["languages"][1] == "English":
-    print('OK')
-else:
-    print('NOT OK')
