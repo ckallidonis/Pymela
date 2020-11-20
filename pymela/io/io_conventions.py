8a3d517fb8eb5d494eaf29bf220bf655e8897e23
Christos Kallidonis <kallidonis@gmail.com>
2020-11-20 17:54:31 -0500

    Added a file with conventions on the JSON input Tags

---
+++ pymela/io/io_conventions.py
@@ -0,0 +1,14 @@
+'''
+Created on Nov.20, 2020
+@author: Christos Kallidonis
+Copyright (C) 2020. All rights reserved.
+
+This file contains definitions of conventions about the keys of the objects(dictionaries) of the JSON-format input files.
+If these conventions are modified, they are also modified just once within the Pymela package
+'''
+
+
+# Main Object Conventions
+analysisInfoTag = "Analysis Info"
+c2ptDataInfoTag = "2pt Data Info"
+ensembleInfoTag = "Ensemble Info"
