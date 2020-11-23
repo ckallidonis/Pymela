8c7d4fb866cc4ecc82cf191d2770482fee29764d
Christos Kallidonis <kallidonis@gmail.com>
2020-11-23 12:45:31 -0500

    Some more I/O Conventions

--- pymela/io/io_conventions.py
+++ pymela/io/io_conventions.py
@@ -9,6 +9,15 @@ If these conventions are modified, they are also modified just once within the P
 
 
 # Main Object Conventions
-analysisInfoTag = "Analysis Info"
-c2ptDataInfoTag = "2pt Data Info"
-ensembleInfoTag = "Ensemble Info"
+analysisInfoTag = 'Analysis Info'
+c2ptDataInfoTag = '2pt Data Info'
+ensembleInfoTag = 'Ensemble Info'
+
+# What is expected in the JSON input file, based on the type of run/test
+inputInfoTags = {'2pt analysis': [analysisInfoTag, c2ptDataInfoTag, ensembleInfoTag]}
+
+# What is expected in each object of the JSON input file
+expectedInput = {c2ptDataInfoTag: ['Data Main Directory', 'Nt', 'Source Operator File', 'Sink Operator File',
+                                   'Source Nrows', 'Sink Nrows', 'Compute X-rows'],
+                 analysisInfoTag: ['Phasing Tag', 't0', 'Momentum', 'Nvec', 'Ncfg'],
+                 ensembleInfoTag: ['Tag', 'L', 'T', 'alat fm', 'mpi GeV', 'mN GeV']}
