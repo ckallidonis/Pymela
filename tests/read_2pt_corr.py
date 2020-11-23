35c1843a7e4b7f1f6801c9fe701c47f625672fb2
Christos Kallidonis <kallidonis@gmail.com>
2020-11-23 12:46:15 -0500

    Make I/O checks in read_2pt_corr.py

--- tests/read_2pt_corr.py
+++ tests/read_2pt_corr.py
@@ -12,15 +12,17 @@ import optparse
 # Add package path to sys.path. This allows us to run this tests script from any directory, without import issues
 file_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(file_path+'/../')
-
-# Avoid writing the compiled files
-sys.dont_write_bytecode = True
+fileName = __file__.split('/')[-1]
 
 # Import local modules
 import pymela.io.json_io as JSONio
-import pymela.io.io_conventions as io_conv
-
+import pymela.io.io_conventions as ioConv
 from pymela.twopointcorr import *
+
+runType = '2pt analysis'
+
+# Avoid writing the compiled files
+sys.dont_write_bytecode = True
 
 
 # Parse command line options
@@ -38,13 +40,18 @@ if input_file == '':
 
 
 # Parse and print out the input file
-iniDict = JSONio.parse(input_file)
-JSONio.dumpDictObject(iniDict,'\n%s - Got the following Input:' %(__file__.split('/')[-1]))
+ioDict = JSONio.parse(input_file)
+JSONio.dumpDictObject(ioDict,'\n%s - Got the following Input:' %(fileName))
+
+# Make cheks on Input data
+for infoTag, infoDict in ioDict.items():
+    JSONio.makeInputChecks(infoTag, infoDict, runType)
 
 
-c2pt_dataInfo = iniDict[io_conv.c2ptDataInfoTag]
-analysisInfo = iniDict[io_conv.analysisInfoTag]
-ensembleInfo = iniDict[io_conv.ensembleInfoTag]
+c2pt_dataInfo = ioDict[ioConv.c2ptDataInfoTag]
+analysisInfo = ioDict[ioConv.analysisInfoTag]
+ensembleInfo = ioDict[ioConv.ensembleInfoTag]
+
 
 
 c2pt = TwoPointCorrelator(dataInfo = c2pt_dataInfo, analysisInfo = analysisInfo)
