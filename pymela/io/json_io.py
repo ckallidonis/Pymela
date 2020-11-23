776a528cd0cb3f2664cbcd909039bb5366556ef6
Christos Kallidonis <kallidonis@gmail.com>
2020-11-23 12:45:48 -0500

    Function to make I/O checks

--- pymela/io/json_io.py
+++ pymela/io/json_io.py
@@ -1,4 +1,5 @@
 import json
+import io_conventions as ioConv
 
 def parse(ini_file):
     with open(ini_file) as f:
@@ -10,4 +11,13 @@ def dumpDictObject(dictObj,printMessage=''):
     if printMessage != '':
         print(printMessage)
     print(json.dumps(dictObj, indent = 2, sort_keys=True))
+#-------------------------------
+
+def makeInputChecks(infoTag,infoDict,runType):
+    if infoTag not in ioConv.inputInfoTags[runType]:
+        raise ValueError('Expected object %s in JSON input file' % (infoTag))
+
+    for input in ioConv.expectedInput[infoTag]:
+        if input not in infoDict:
+            raise ValueError('Expected entry "%s" in object "%s" of JSON input file' % (input,infoTag))
 #-------------------------------
