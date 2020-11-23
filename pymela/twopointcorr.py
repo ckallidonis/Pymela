f82499d46010eec3522d087c509a903dc275f329
Christos Kallidonis <kallidonis@gmail.com>
2020-11-23 13:53:18 -0500

    Some comments on the TwoPointCorrelator class; defined dictionaries for data

--- pymela/twopointcorr.py
+++ pymela/twopointcorr.py
@@ -8,10 +8,30 @@ Class definition that holds data and manipulates Two-point correlation functions
 
 import io.json_io as JSONio
 
+# The class holding the two-point correlation functions
+# Two-point functions have dependence on the following attributes:
+#   the source time, t0
+#   the source and sink operators
+#   the rows of the source-sink operators
+#   the Momentum
 class TwoPointCorrelator():
     def __init__(self, dataInfo, analysisInfo):
         self.dataInfo = dataInfo
         self.analysisInfo = analysisInfo
 
+        # Data containers
+        # "plain" means not averaged over t0,src-snk operators, rows, or momentum, i.e. there's depedence on these attributes
+        self.plainData = {}    # The data that is read/loaded
+        self.plainBins = {}    # The Jackknife sampling bins of the plain data
+        self.plainMean = {}    # The Jackknife mean of the plain data
+        
+        self.bins = {}     # The Jackknife sampling bins of the averaged data
+        self.mean = {}     # The Jackknife mean of the averaged data
+        self.stdMean = {}  # The standard mean of the averaged data
+        
+        self.covMean = {}  # Average over all attributes but t0, needed for the Covariant Matrix
+    #-------------------------------
+
     def printInfo(self):
         JSONio.dumpDictObject(self.dataInfo, '\nTwoPointCorrelator - Got the following 2pt data Info:')
+    #-------------------------------
