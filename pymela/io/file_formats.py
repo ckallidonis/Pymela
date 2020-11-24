a8a503007d66112590d5648ff75c33fdb215ce71
Christos Kallidonis <kallidonis@gmail.com>
2020-11-24 16:43:58 -0500

    I/O file that defines directory and file formats

---
+++ pymela/io/file_formats.py
@@ -0,0 +1,21 @@
+'''
+Created on Nov.24, 2020
+@author: Christos Kallidonis
+Copyright (C) 2020. All rights reserved.
+
+Contains functions that define the directory and file format of input/output files
+'''
+
+def getTwoPointFileASCII(phM,t0Tag,src,snk,srow,mFTag,nvec):
+        filePre = 'corr_2pt.baryon.n%d'%(nvec)
+    
+        srcTag = 'src_%s_%d'%(src,srow)
+        snkTag = 'snk_%s_%d'%(snk,srow)
+    
+        FileName = '%s.%s.%s.%s.%s.%s.dat'%(filePre,phM,t0Tag,srcTag,snkTag,mFTag)
+    
+        return FileName
+#--------------------------
+
+def getTwoPointDirASCII(mainDir,t0Tag,mFTag):
+        return '%s/%s/%s' % (mainDir,t0Tag,mFTag)
