7c6df44b6f5ac8829b3c876c96de42518bacfb5b
Christos Kallidonis <kallidonis@gmail.com>
2020-12-07 10:57:32 -0500

    Support for reading three-point functions from JSON files

--- pymela/io/io_conventions.py
+++ pymela/io/io_conventions.py
@@ -11,21 +11,27 @@ If these conventions are modified, they are also modified just once within the P
 # Main Object Conventions
 analysisInfoTag = 'Analysis Info'
 c2ptDataInfoTag = '2pt Data Info'
+c3ptDataInfoTag = '3pt Data Info'
 ensembleInfoTag = 'Ensemble Info'
 effEnergyInfoTag = 'Effective Energy Info'
 
 # What is expected in the JSON input file, based on the type of run/test
 inputInfoTags = {'2pt analysis': [analysisInfoTag, c2ptDataInfoTag, ensembleInfoTag],
+                 '3pt analysis': [analysisInfoTag, c3ptDataInfoTag, ensembleInfoTag], 
                  'Effective Energy Analysis': [analysisInfoTag, c2ptDataInfoTag, ensembleInfoTag, effEnergyInfoTag]}
 
 # What is expected in each object of the JSON input file
 expectedKeys = {c2ptDataInfoTag:  ['Data Main Directory', 'Datasets', 'Data Source', "Write HDF5 Output", "Momentum Average"],
-                 analysisInfoTag: ['Phasing Tag', 'Nvec', 'Binsize'],
-                 ensembleInfoTag: ['Tag', 'L', 'T', 'alat fm', 'mpi GeV', 'mN GeV'],
-                 effEnergyInfoTag: ["HDF5 Output File", "Fitting"]}
+                c3ptDataInfoTag:  ['Data Main Directory', 'Datasets', 'Data Source', "Write HDF5 Output", "Momentum Average"],
+                analysisInfoTag:  ['Phasing Tag', 'Nvec', 'Binsize'],
+                ensembleInfoTag:  ['Tag', 'L', 'T', 'alat fm', 'mpi GeV', 'mN GeV'],
+                effEnergyInfoTag: ["HDF5 Output File", "Fitting"]}
 
 expectedSubKeys = {c2ptDataInfoTag: {'Datasets': ['mom', 'Ncfg', 't0','Nt','Interpolating Operators File',
                                                   'Nrows','Compute X-rows']
                                     },
+                   c3ptDataInfoTag: {'Datasets': ['mom', 'Ncfg', 't0','tsep','disp','Interpolating Operators File',
+                                                  'Insertion Operators', 'Nrows','Compute X-rows']
+                                    },                 
                    effEnergyInfoTag: {'Fitting': ['Type', 'Ranges']}
                   }
